8c7987f67ad21d752741005165e521aebf8dc4a5
Ousmane Sarr Diop <ousmane.diop.1@etu.u-bordeaux.fr>
2025-05-01 21:43:58 +0200

    typage simple reuissit

--- mini_ml/_build/default/language/typer_util.ml
+++ mini_ml/_build/default/language/typer_util.ml
@@ -18,8 +18,26 @@ end
 let type_of_built_in (built_in : built_in) =
   match built_in with
   | Add -> TFunc ([], TInt, TFunc ([], TInt, TInt))
-  (* à modifier après ça*)
-  | _ -> failwith "built_in not yet typed"
+  | Sub -> TFunc ([], TInt, TFunc ([], TInt, TInt))
+  | Mul -> TFunc ([], TInt, TFunc ([], TInt, TInt))
+  | Div -> TFunc ([], TInt, TFunc ([], TInt, TInt))
+  | Mod -> TFunc ([], TInt, TFunc ([], TInt, TInt))
+  | UMin -> TFunc ([], TInt, TInt)
+  | And -> TFunc ([], TBool, TFunc ([], TBool, TBool))
+  | Or -> TFunc ([], TBool, TFunc ([], TBool, TBool))
+  | Not -> TFunc ([], TBool, TBool)
+  | Eq -> let t = TUniv (-1) in TFunc ([t], t, TFunc ([], t, TBool))
+  | Neq -> let t = TUniv (-1) in TFunc ([t], t, TFunc ([], t, TBool))
+  | Lt -> TFunc ([], TInt, TFunc ([], TInt, TBool))
+  | Gt -> TFunc ([], TInt, TFunc ([], TInt, TBool))
+  | Leq -> TFunc ([], TInt, TFunc ([], TInt, TBool))
+  | Geq -> TFunc ([], TInt, TFunc ([], TInt, TBool))
+  | Concat -> TFunc ([], TString, TFunc ([], TString, TString))
+  | Cat -> let t = TUniv (-1) in TFunc ([t], t, TFunc ([], TList ([], t), TList ([], t)))
+  | Append -> let t = TUniv (-1) in TFunc ([t], TList ([], t), TFunc ([], TList ([], t), TList ([], t)))
+  | Head -> let t = TUniv (-1) in TFunc ([t], TList ([], t), t)
+  | Tail -> let t = TUniv (-1) in TFunc ([t], TList ([], t), TList ([], t))
+  | Print -> let t = TUniv (-1) in TFunc ([t], t, TUnit)
 
 let rec solve_constraints (constraints : (type_lang * type_lang) list) :
     (int * type_lang) list =
