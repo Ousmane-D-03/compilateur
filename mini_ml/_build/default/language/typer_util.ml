59821bd2aaab4787b1618e3c736a20f4aa6fe769
Ousmane Sarr Diop <ousmane.diop.1@etu.u-bordeaux.fr>
2025-05-01 22:17:28 +0200

    fin du projet

--- mini_ml/_build/default/language/typer_util.ml
+++ mini_ml/_build/default/language/typer_util.ml
@@ -4,6 +4,14 @@ open Ast
 exception Constraint_error of type_lang * type_lang
 exception Typing_error of Util.Position.t * string
 
+(* Déplacer occurs_in avant son utilisation *)
+let rec occurs_in (n : int) (t : type_lang) : bool =
+  match t with
+  | TUniv m -> n = m
+  | TList(_, t) -> occurs_in n t
+  | TFunc(_, t1, t2) -> occurs_in n t1 || occurs_in n t2
+  | _ -> false
+
 module Counter = struct
   type t = int ref
 
@@ -23,33 +31,47 @@ let type_of_built_in (built_in : built_in) =
   | Div -> TFunc ([], TInt, TFunc ([], TInt, TInt))
   | Mod -> TFunc ([], TInt, TFunc ([], TInt, TInt))
   | UMin -> TFunc ([], TInt, TInt)
-  | And -> TFunc ([], TBool, TFunc ([], TBool, TBool))
-  | Or -> TFunc ([], TBool, TFunc ([], TBool, TBool))
+  | Cat -> 
+      let t = TUniv 0 in 
+      TFunc ([], t, TFunc ([], TList([], t), TList([], t)))
+  | And | Or -> TFunc ([], TBool, TFunc ([], TBool, TBool))
   | Not -> TFunc ([], TBool, TBool)
-  | Eq -> let t = TUniv (-1) in TFunc ([t], t, TFunc ([], t, TBool))
-  | Neq -> let t = TUniv (-1) in TFunc ([t], t, TFunc ([], t, TBool))
-  | Lt -> TFunc ([], TInt, TFunc ([], TInt, TBool))
-  | Gt -> TFunc ([], TInt, TFunc ([], TInt, TBool))
-  | Leq -> TFunc ([], TInt, TFunc ([], TInt, TBool))
-  | Geq -> TFunc ([], TInt, TFunc ([], TInt, TBool))
+  | Eq | Neq | Lt | Gt | Leq | Geq -> 
+      let t = TUniv 0 in
+      TFunc ([], t, TFunc ([], t, TBool))
+  | Print -> let t = TUniv 0 in TFunc ([], t, TUnit)
+  | Head -> let t = TUniv 0 in TFunc ([], TList([], t), t)
+  | Tail -> let t = TUniv 0 in TFunc ([], TList([], t), TList([], t))
   | Concat -> TFunc ([], TString, TFunc ([], TString, TString))
-  | Cat -> let t = TUniv (-1) in TFunc ([t], t, TFunc ([], TList ([], t), TList ([], t)))
-  | Append -> let t = TUniv (-1) in TFunc ([t], TList ([], t), TFunc ([], TList ([], t), TList ([], t)))
-  | Head -> let t = TUniv (-1) in TFunc ([t], TList ([], t), t)
-  | Tail -> let t = TUniv (-1) in TFunc ([t], TList ([], t), TList ([], t))
-  | Print -> let t = TUniv (-1) in TFunc ([t], t, TUnit)
+  | Append -> 
+      let t = TUniv 0 in 
+      TFunc ([], TList([], t), TFunc ([], TList([], t), TList([], t)))
 
 let rec solve_constraints (constraints : (type_lang * type_lang) list) :
     (int * type_lang) list =
   match constraints with
-  (* à modifier après ça*)
-  | [] -> failwith "solve_constraints not implemented"
-  | ((TFunc _ as f), TBool) :: _ -> raise (Constraint_error (f, TBool))
-  | _ :: t ->
-      ignore substitute_constraint;
-      solve_constraints t
+  | [] -> []
+  | (t1, t2) :: rest ->
+      match (t1, t2) with
+      | TUniv n, t | t, TUniv n ->
+          if occurs_in n t then
+            raise (Constraint_error (t1, t2))
+          else
+            let rest' = List.map 
+              (fun (a,b) -> substitute_constraint n t (a, b)) rest in
+            (n, t) :: solve_constraints rest'
+            
+      | TList(_, a1), TList(_, a2) ->
+          solve_constraints ((a1, a2) :: rest)
+          
+      | TFunc(_, a1, r1), TFunc(_, a2, r2) ->
+          solve_constraints ((a1, a2) :: (r1, r2) :: rest)
+          
+      | t1, t2 when t1 = t2 ->
+          solve_constraints rest
+          
+      | _ -> raise (Constraint_error (t1, t2))
 
 let instantiate (counter : Counter.t) (type_lang : type_lang) : type_lang =
-  (* à modifier*)
   let _ = Counter.get_fresh counter in
   match type_lang with _ -> type_lang
--- mini_ml/language/typer.ml
+++ mini_ml/language/typer.ml
@@ -4,8 +4,38 @@ open Typer_util
 
 let rec type_expr (counter : Counter.t) (env : type_lang Util.Environment.t)
     (expr : expr) =
-  (* la suite est à modifier -- c’est juste là pour ne pas avoir de warning tant que vous ne travaillez pas dessus.*)
-  ignore generalize_type_expr;
   match expr with
-  | App (e, _, _) -> type_expr counter env e
-  | _ -> failwith "full typer not implemented"
+  | App(e1, e2, ann) ->
+      let (t1, c1) = type_expr counter env e1 in
+      let (t2, c2) = type_expr counter env e2 in
+      let t1' = instantiate counter t1 in
+      let t2' = instantiate counter t2 in
+      let t_res = TUniv(Counter.get_fresh counter) in
+      let constraints = (t1', TFunc([], t2', t_res)) :: (c1 @ c2) in
+      Annotation.set_type ann t_res;
+      (t_res, constraints)
+
+  | Let(rec_flag, x, e1, e2, ann) ->
+      let count_before = Counter.get_fresh counter in
+      let (t1, c1) = 
+        if rec_flag then
+          let t_var = TUniv(Counter.get_fresh counter) in
+          Util.Environment.add env x t_var;
+          let (t, c) = type_expr counter env e1 in
+          (t, (t_var, t) :: c)
+        else type_expr counter env e1 
+      in
+      let (internal, external_) = split_constraint_by_floor count_before c1 in
+      let subst = solve_constraints internal in
+      let t1' = apply_subst_in_type subst t1 in
+      (* Création d'une expression temporaire pour la généralisation *)
+      let temp_ann = Annotation.create Lexing.(dummy_pos, dummy_pos) in
+      let () = Annotation.set_type temp_ann t1' in
+      let temp_expr = Unit temp_ann in
+      let t1'' = generalize_type_expr count_before temp_expr in
+      Util.Environment.add env x t1'';
+      let (t2, c2) = type_expr counter env e2 in
+      Util.Environment.remove env x;
+      (t2, external_ @ c2)
+
+  | _ -> Typer_naive.type_expr counter env expr
