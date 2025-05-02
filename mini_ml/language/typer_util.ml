7b74c7846ad241b17c4debafa762a5e2be9146c0
Ousmane Sarr Diop <ousmane.diop.1@etu.u-bordeaux.fr>
2025-05-02 07:37:18 +0200

    Revert "fin du projet"
    This reverts commit 59821bd2aaab4787b1618e3c736a20f4aa6fe769.

--- mini_ml/language/typer_util.ml
+++ mini_ml/language/typer_util.ml
@@ -4,14 +4,6 @@ open Ast
 exception Constraint_error of type_lang * type_lang
 exception Typing_error of Util.Position.t * string
 
-(* Déplacer occurs_in avant son utilisation *)
-let rec occurs_in (n : int) (t : type_lang) : bool =
-  match t with
-  | TUniv m -> n = m
-  | TList(_, t) -> occurs_in n t
-  | TFunc(_, t1, t2) -> occurs_in n t1 || occurs_in n t2
-  | _ -> false
-
 module Counter = struct
   type t = int ref
 
@@ -26,52 +18,20 @@ end
 let type_of_built_in (built_in : built_in) =
   match built_in with
   | Add -> TFunc ([], TInt, TFunc ([], TInt, TInt))
-  | Sub -> TFunc ([], TInt, TFunc ([], TInt, TInt))
-  | Mul -> TFunc ([], TInt, TFunc ([], TInt, TInt))
-  | Div -> TFunc ([], TInt, TFunc ([], TInt, TInt))
-  | Mod -> TFunc ([], TInt, TFunc ([], TInt, TInt))
-  | UMin -> TFunc ([], TInt, TInt)
-  | Cat -> 
-      let t = TUniv 0 in 
-      TFunc ([], t, TFunc ([], TList([], t), TList([], t)))
-  | And | Or -> TFunc ([], TBool, TFunc ([], TBool, TBool))
-  | Not -> TFunc ([], TBool, TBool)
-  | Eq | Neq | Lt | Gt | Leq | Geq -> 
-      let t = TUniv 0 in
-      TFunc ([], t, TFunc ([], t, TBool))
-  | Print -> let t = TUniv 0 in TFunc ([], t, TUnit)
-  | Head -> let t = TUniv 0 in TFunc ([], TList([], t), t)
-  | Tail -> let t = TUniv 0 in TFunc ([], TList([], t), TList([], t))
-  | Concat -> TFunc ([], TString, TFunc ([], TString, TString))
-  | Append -> 
-      let t = TUniv 0 in 
-      TFunc ([], TList([], t), TFunc ([], TList([], t), TList([], t)))
+  (* à modifier après ça*)
+  | _ -> failwith "built_in not yet typed"
 
 let rec solve_constraints (constraints : (type_lang * type_lang) list) :
     (int * type_lang) list =
   match constraints with
-  | [] -> []
-  | (t1, t2) :: rest ->
-      match (t1, t2) with
-      | TUniv n, t | t, TUniv n ->
-          if occurs_in n t then
-            raise (Constraint_error (t1, t2))
-          else
-            let rest' = List.map 
-              (fun (a,b) -> substitute_constraint n t (a, b)) rest in
-            (n, t) :: solve_constraints rest'
-            
-      | TList(_, a1), TList(_, a2) ->
-          solve_constraints ((a1, a2) :: rest)
-          
-      | TFunc(_, a1, r1), TFunc(_, a2, r2) ->
-          solve_constraints ((a1, a2) :: (r1, r2) :: rest)
-          
-      | t1, t2 when t1 = t2 ->
-          solve_constraints rest
-          
-      | _ -> raise (Constraint_error (t1, t2))
+  (* à modifier après ça*)
+  | [] -> failwith "solve_constraints not implemented"
+  | ((TFunc _ as f), TBool) :: _ -> raise (Constraint_error (f, TBool))
+  | _ :: t ->
+      ignore substitute_constraint;
+      solve_constraints t
 
 let instantiate (counter : Counter.t) (type_lang : type_lang) : type_lang =
+  (* à modifier*)
   let _ = Counter.get_fresh counter in
   match type_lang with _ -> type_lang
